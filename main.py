3e8b10dc229b0d3d20585572858e15eed316fc59
Kidus Mekonnen <kidusmekonnen591@gmail.com>
2024-04-19 10:17:41 +0300

    fixed prompt after wrong move and argument validation

--- main.py
+++ main.py
@@ -11,15 +11,38 @@ def parse_arguments():
     undo_redo_enabled = False
     score_display = False
 
+    player_types = [
+        "human",
+        "heuristic",
+        "random"
+    ]
+
+
     # Assign values based on command line arguments
     if len(sys.argv) > 1:
-        player1_type = sys.argv[1].lower()
+        if(sys.argv[1].lower() in player_types):
+            player1_type = sys.argv[1].lower()
+        else:
+            print("Invalid player type for white player")
+            exit()
     if len(sys.argv) > 2:
-        player2_type = sys.argv[2].lower()
+        if(sys.argv[2].lower() in player_types):
+            player2_type = sys.argv[2].lower()
+        else:
+            print("Invalid player type for blue player")
+            exit()
     if len(sys.argv) > 3:
-        undo_redo_enabled = sys.argv[3].lower() == 'on'
+        if(sys.argv[3].lower() == 'on' or sys.argv[3].lower() == 'off'):
+            undo_redo_enabled = sys.argv[3].lower() == 'on'
+        else:
+            print("Invalid value for enable undo/redo (on/off)")
+            exit()
     if len(sys.argv) > 4:
-        score_display = sys.argv[4].lower() == 'on'
+        if(sys.argv[3].lower() == 'on' or sys.argv[3].lower() == 'off'):
+            score_display = sys.argv[4].lower() == 'on'
+        else:
+            print("Invalid value for enable score display (on/off)")
+            exit()
 
     return player1_type, player2_type, undo_redo_enabled, score_display
 
@@ -33,7 +56,7 @@ def main():
         game.play_turn()
 
     # Option to restart the game based on user input
-    restart = input("Play again?\n")
+    restart = input("Play again (yes/no)?\n")
     if restart.lower() == 'yes':
         main()
 
